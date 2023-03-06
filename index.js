3280e9bf319193d797a416b2b303b479b6955fad
Zeniat Mohammed <zeniatmohammed10@gmail.com>
2023-03-06 23:32:12 +0100

    js update

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 var quotes = [
     
-    ["\"Be in this world like a stranger\"", " - مـحـمـد رسـول الله"],
+    ["\"Be in this world like a stranger\"", " - رسول الله صلى الله عليه وسلم"],
     ["\"Verily, with every hardship comes ease.\"", " - Qur'an 94:6"],
     ["\"Allah is with the patient.\"", " - Qur'an 2:153"],
-    ["\"Speak good or remain silent.\"", " - مـحـمـد رسـول الله"],
-    ["\"The best of people are those who are most beneficial to others.\"", " - مـحـمـد رسـول الله"],
-    ["\"A true Muslim is one from whose tongue and hands mankind is safe.\"", " - مـحـمـد رسـول الله"],
-    ["\"The best among you are those who have the best manners and character.\"", " - مـحـمـد رسـول الله"],
+    ["\"Speak good or remain silent.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"The best of people are those who are most beneficial to others.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"A true Muslim is one from whose tongue and hands mankind is safe.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"The best among you are those who have the best manners and character.\"", " - رسول الله صلى الله عليه وسلم"],
     ["\"Allah loves those who repent and purify themselves.\"", " - Qur'an 2:222"],
     ["\"Verily, Allah is with the patient.\"", " - Qur'an 2:153"],
-    ["\"None of you truly believes until he loves for his brother what he loves for himself.\"", " - مـحـمـد رسـول الله"],
+    ["\"None of you truly believes until he loves for his brother what he loves for himself.\"", " - رسول الله صلى الله عليه وسلم"],
     ["\"Allah is with those who restrain themselves.\"", " - Qur'an 16:128"],
-    ["\"A believer does not taunt, curse, abuse or talk indecently.\"", " - مـحـمـد رسـول الله"],
-    ["\"The strongest among you is the one who controls his anger.\"", " - مـحـمـد رسـول الله"],
-    ["\"The best richness is the richness of the soul.\"", " - مـحـمـد رسـول الله"],
-    ["\"Heaven lies beneath the feet of your mother.\"", " - مـحـمـد رسـول الله"],
-    ["\"Treat your neighbors with kindness.\"", " - مـحـمـد رسـول الله"],
-    ["\"Keep peace and good relations between people.\"", " - مـحـمـد رسـول الله"],
+    ["\"A believer does not taunt, curse, abuse or talk indecently.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"The strongest among you is the one who controls his anger.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"The best richness is the richness of the soul.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"Heaven lies beneath the feet of your mother.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"Treat your neighbors with kindness.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"Keep peace and good relations between people.\"", " - رسول الله صلى الله عليه وسلم"],
     ["\"In the remembrance of Allah do hearts find rest.\"", " - Qur'an 13:28"],
-    ["\"The best among you are those who are the best to their wives.\"", " - مـحـمـد رسـول الله"],
-    ["\"Purification is half of faith.\"", " - صلى الله عليه وسلم  ]
+    ["\"The best among you are those who are the best to their wives.\"", " - رسول الله صلى الله عليه وسلم"],
+    ["\"Purification is half of faith.\"", " - رسول الله صلى الله عليه وسلم"]
 
 ]
 
